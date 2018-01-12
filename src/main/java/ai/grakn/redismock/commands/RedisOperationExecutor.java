aa857b848cbb98820b36aee33cf03ae792420a7c
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2018-01-12 17:54:39 +0000

    Fix one test

--- src/main/java/ai/grakn/redismock/commands/RedisOperationExecutor.java
+++ src/main/java/ai/grakn/redismock/commands/RedisOperationExecutor.java
@@ -125,6 +125,10 @@ public class RedisOperationExecutor {
                 return new RO_ping(base, params);
             case "keys":
                 return new RO_keys(base, params);
+            case "sadd":
+                return new RO_sadd(base, params);
+            case "smembers":
+                return new RO_smembers(base, params);
             default:
                 throw new UnsupportedOperationException(String.format("Unsupported operation '%s'", name));
         }
